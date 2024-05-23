3e7127a102d3e44ba15ec464bda18a6c2307d569
Espen Sivertsen Oftedal <98535233+Essivoft@users.noreply.github.com>
2024-05-23 11:34:14 +0200

    Add files via upload

--- ckanext/my_landing_page/plugin.py
+++ ckanext/my_landing_page/plugin.py
@@ -1,10 +1,12 @@
 import ckan.plugins as plugins
 import ckan.plugins.toolkit as toolkit
 from ckanext.my_landing_page.controller.landing_page import MyController
+from ckan.model.group import Group
 
 class MyLandingPagePlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.IRoutes, inherit=True)
+    plugins.implements(plugins.ITemplateHelpers)
 
     def update_config(self, config):
         toolkit.add_template_directory(config, 'templates')
@@ -14,3 +16,13 @@ class MyLandingPagePlugin(plugins.SingletonPlugin):
     def before_map(self, map):
         map.connect('home', '/', controller='ckanext.my_landing_page.controller.landing_page:MyController', action='index')
         return map
+
+    def get_helpers(self):
+        return {
+            'my_group_list': self.my_group_list
+        }
+
+    def my_group_list(self, limit=20):
+        context = {'model': Group, 'session': Group.session, 'user': toolkit.c.user}
+        data_dict = {'all_fields': True, 'limit': limit}
+        return toolkit.get_action('group_list')(context, data_dict)
